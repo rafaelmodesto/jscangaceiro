fa0b3fb55959b8afb20878ecc07c48a3ab814eaf
Rafael Modesto <rafaelmodesto09@gmail.com>
2019-10-17 23:19:18 -0300

    COMMIT #2 - Evolução da view cap 6.2

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -10,7 +10,10 @@ class NegociacaoController{
     this._inputQuantidade = $('#quantidade');
     this._inputValor      = $('#valor');
     this._negociacoes     = new Negociacoes();
-    this._negociacoesView = new NegociacoesView();    
+    this._negociacoesView = new NegociacoesView('#negociacoes');
+    
+    //atualizando a view
+    this._negociacoesView.update();
 
   }
 
