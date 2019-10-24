067262a5de0959d1bfeb2ef1311a2466245eeab5
Rafael Modesto <rafaelmodesto09@gmail.com>
2019-10-23 21:22:36 -0300

    COMMIT #4 - Finalizado cap6.

--- client/app/ui/views/NegociacoesView.js
+++ client/app/ui/views/NegociacoesView.js
@@ -21,19 +21,22 @@ class NegociacoesView {
       </thead>
 
       <tbody>
-        ${model.paraArray().map(negociacao =>{
-          return `
+        ${model.paraArray().map(negociacao =>
+        `
           <tr>
             <td>${DateConverter.paraTexto(negociacao.data)}</td>
             <td>${negociacao.quantidade}</td>
             <td>${negociacao.valor}</td>
             <td>${negociacao.volume}</td>
           </tr>
-          `
-        }).join('')}
+        `).join('')}
       </tbody>
       
       <tfoot>
+          <tr>
+            <td colspan="3"></td>
+            <td>${model.volumeTotal}</td>
+          </tr>
       </tfoot>
     </table>
     `
