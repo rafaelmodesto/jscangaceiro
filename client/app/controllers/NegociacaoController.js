538713fdb70397647096f15cd108a14b82c52235
Rafael Modesto <rafaelmodesto09@gmail.com>
2019-11-05 21:40:59 -0300

    COMMIT #8 - Finalizando cap9.

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -14,7 +14,25 @@ class NegociacaoController{
       console.log(this);
       this._negociacoesView.update(model);
     });
-   */ 
+   */
+    const self = this;
+
+    this._negociacoes = new Proxy(new Negociacoes(), {
+      get(target, prop, receiver) {
+        if(typeof(target[prop]) == typeof(Function) && ['adiciona', 'esvazia']
+        .includes(prop)) {
+          return function() {
+            console.log(`"${prop}" disparou a armadilha`);
+            target[prop].apply(target, arguments);
+
+            self._negociacoesView.update(target);
+          }
+        } else {
+          return target[prop];
+        }
+      }
+    });
+   
     this._negociacoesView = new NegociacoesView('#negociacoes');
     
     //atualizando a view
